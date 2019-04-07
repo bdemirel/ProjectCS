d9755621251db0961b0c565771c38e0dcd37e3bb
Baris Demirel <bdemirel732@gmail.com>
2019-04-07 14:24:18 +0200

    Changed to single process to push into db

--- decompression.sh
+++ decompression.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-count=$(ls -R -l /data/$USER/results/$2/parse | grep .*.json | wc -l)
+file=$(ls /data/$USER/results/$2 | grep $1.*.tar.gz | head -1)
 
-if [ $count -lt 12 ]
+if [ -f "/data/$USER/results/$2/$file" ]
 then
-	file=$(ls /data/$USER/results/$2 | grep $1.*.tar.gz | head -1)
-	
-	if [ -f "/data/$USER/results/$2/$file" ]
-	then
-		tar -C "/data/$USER/results/$2/parse" -xvzf "/data/$USER/results/$2/$file" --xform='s#^.+/##x'
-		mv "/data/$USER/results/$2/$file" "/data/$USER/results/$2/decomp"
-	fi
+	tar -C "/data/$USER/results/$2/parse" -xvzf "/data/$USER/results/$2/$file" --xform='s#^.+/##x'
+	mv "/data/$USER/results/$2/$file" "/data/$USER/results/$2/decomp"
 fi
--- p2.py
+++ p2.py
@@ -1,8 +1,5 @@
-import mysql.connector as DBCon, logging, sys, glob, getpass, json, getopt, subprocess, itertools, os
-from mysql.connector import pooling
+import mysql.connector as DBCon, logging, sys, glob, getpass, json, getopt, subprocess, os
 from mysql.connector import Error
-from multiprocessing import Pool
-from itertools import zip_longest
 
 #logging
 logger = logging.getLogger('main')
@@ -20,90 +17,73 @@ fh.setFormatter(formatter)
 logger.addHandler(ch)
 logger.addHandler(fh)
 
-#start database pool
+#start database connection
 try:
-	global dbpool
-	dbpool = DBCon.pooling.MySQLConnectionPool(pool_name="dbpool", pool_size=7, option_files='mysql_options.cnf')	
-except:
+	conn = DBCon.connect(option_files="mysql_options.cnf")
+except Error as err:
 	logger.info("Cannot connect to MySQL!")
-	#logger.info(err)
+	logger.Error(str(err))
 	sys.exit(1)
-logger.info("DB Connection Pool started")
+logger.info("DB Connection started")
 
-def parse(dyear, jsonfile):
-	logger.info("File started: %s", jsonfile)
-	conn = dbpool.get_connection()
-	print(conn.is_connected())
-	if (not conn.is_connected()):
-		return(0, 0)
-	else:
-		cursor = conn.cursor(prepared=True)
-		
-		stmtCDN = "INSERT INTO cdn"+str(dyear)+" (`domain`, `count`) VALUES (?, ?)"
-		cursor.execute(stmtCDN)
-		stmtAll = "INSERT INTO `"+str(dyear)+"` (query_name, query_type, response_name, response_type, cname, dname, timestamp, ipv4, ipv6, as_full, country, cdn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-		cursor.execute(stmtAll)
+#parse commandline arguments
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hoy:")
+except getopt.GetoptError as err:
+	logger.error(str(err))
+	sys.exit(1)
+
+dataset = "alexa1m"
+for opt, val in opts:
+	if opt == "-h":
+		print("p2.py (-o) -y <year-to-parse>")
+		sys.exit()
+	elif opt == "-o":
+		dataset = "open-tld"
+	elif opt == "-y":
+		parseyear = int (val)
+if not 'parseyear' in locals():
+	logger.info("Missing year!")
+	sys.exit(1)
+
+#set variables and start processing
+results = []
+dcount = ccount = 0
+while True:
+	#check for files
+	batch = glob.glob(os.path.join("/data", getpass.getuser(), "results", dataset, "parse", "*.json"))
+	if batch == []:
+		if glob.glob(os.path.join("/data", getpass.getuser(), "results", dataset, str(parseyear)+"-*.tar.gz")) != []:
+			subprocess.run(['./decompression.sh', str(parseyear), dataset], check=True)
+			batch = glob.glob(os.path.join("/data", getpass.getuser(), "results", dataset, "parse", "*.json"))
+		else:
+			break
+	
+	#prepare db statements
+	cursor = conn.cursor(prepared=True)
+	stmtCDN = "INSERT INTO cdn"+str(parseyear)+" (`domain`, `count`) VALUES (?, ?)"
+	cursor.execute(stmtCDN)
+	stmtAll = "INSERT INTO `"+str(parseyear)+"` (query_name, query_type, response_name, response_type, cname, dname, timestamp, ipv4, ipv6, as_full, country, cdn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	cursor.execute(stmtAll)
+
+	#process the files
+	for jsonfile in batch:
+		logger.info("File started: %s", jsonfile)
 		with open(jsonfile) as jf:
 			contents = json.load(jf)
 			for row in contents:
 				if "_id" in row:
 					cursor.execute(stmtAll, (row["query_name"], row["query_type"], row["response_name"], row["response_type"], row["cname_name"], row["dname_name"], row["timestamp"]/1000, row["ip4_address"], row["ip6_address"], row["as_full"], row["country"], row["_cdn"]))
 				elif "dname_count" in row:
-					dcount = row["dname_count"]
-					ccount = row["cname_count"]
+					dcount += row["dname_count"]
+					ccount += row["cname_count"]
 				else:
 					for site, cnt in row.items():
 						cursor.execute(stmtCDN, (site, cnt))
-		conn.close()
 		jf.close()
 		os.remove(jsonfile)
-		return (dcount, ccount)
 
-def main():
-	#parse commandline arguments
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hoy:")
-	except getopt.GetoptError as err:
-		logger.info(str(err))
-		sys.exit(1)
-	
-	dataset = "alexa1m"
-	for opt, val in opts:
-		if opt == "-h":
-			print("p2.py (-o) -y <year-to-parse>")
-			sys.exit()
-		elif opt == "-o":
-			dataset = "open-tld"
-		elif opt == "-y":
-			parseyear = int (val)
-	if not 'parseyear' in locals():
-		logger.info("Missing year!")
-		sys.exit(1)
-
-	#set arguments, start and run multiprocessing pool
-	pool = Pool(processes=7, maxtasksperchild=1)
-	subprocess.run(['./decompression.sh', str(parseyear), dataset], check=True)
-	results = []
-	dcount = ccount = 0
-	while True:
-		batch = glob.glob(os.path.join("/data", getpass.getuser(), "results", dataset, "parse", "*.json"))[:7]
-		if batch == []:
-			break
-		arglist = [(parseyear, x) for x in batch]
-		results += pool.starmap(parse, arglist, 1)
-		subprocess.run(['./decompression.sh', str(parseyear), dataset], check=True)
-		sys.exit(0)
-	"""for batch in batcher(glob.glob(os.path.join("/data", getpass.getuser(), "results", dataset, "parse", "*.json")), 12):
-		arglist = []
-		for file in batch:
-			arglist.append((parseyear, file))
-		pool.starmap(parse, arglist, 1)
-		subprocess.run(['./decompression.sh', str(parseyear), dataset], check=True)"""
-	for dnames, cnames in results:
-		dcount += dnames
-		ccount += cnames
-	logger.info("Domain names imported: %s", dcount)
-	logger.info("CDN domains imported: %s", ccount)
-
-if __name__ == "__main__":
-	main()
+#close connection and print results
+conn.close()
+logger.info("Domain names imported: %s", dcount)
+logger.info("CDN domains imported: %s", ccount)
